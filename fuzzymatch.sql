278f6ae8da4ffc352d1c62a177c3f018de6d14b0
Max Voelker <Chmavo@users.noreply.github.com>
2017-03-26 17:56:06 -0400

    Update fuzzymatch.sql

--- fuzzymatch.sql
+++ fuzzymatch.sql
@@ -1,4 +1,4 @@
-select A.business_id A.name, A.cate gories, A.attributes, A.hours, A.stars, A.review_count, A.is_open,  A.longitude, A.latitude, A.address, A.city, A.postal_code, A.neighborhood, B._id, b.facility_name, b.encounter, b.id, b.placard_st, b.bus_st_date, b.inspect_dt, b.start_time, b.end_time, b.description, b.num || b.street as b_address, b.city,  b.rating, b.description_new, b.low, b.medium, b.high
+select A.business_id A.name, A.categories, A.attributes, A.hours, A.stars, A.review_count, A.is_open,  A.longitude, A.latitude, A.address, A.city, A.postal_code, A.neighborhood, B._id, b.facility_name, b.encounter, b.id, b.placard_st, b.bus_st_date, b.inspect_dt, b.start_time, b.end_time, b.description, b.num || b.street as b_address, b.city,  b.rating, b.description_new, b.low, b.medium, b.high
 from restaurants A
 inner join public.violations B
 on levenshtein(a.name, b.facility_name) <3
